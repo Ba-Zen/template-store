33ca67fae1417def8cf0451518118ebbc4c54163
bazen <bazenatlas@gmail.com>
2023-11-18 16:02:18 -0500

    product and temp data

--- app/components/home-page.tsx
+++ app/components/home-page.tsx
@@ -1,16 +1,61 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Balancer from 'react-wrap-balancer'
-export default function HomePage() {
+
+interface Template {
+  name: string
+  price: string
+  category: string
+  img: string
+}
+
+interface Props {
+  templates: Template[]
+}
+export default function HomePage({ templates }: Props) {
   return (
-    <div className='pt-24 px-4'>
-      <h1 className='text-3xl md:text-[5vw] md:leading-[1.1] font-semibold tracking-tight pb-6'>
-        <Balancer>
-          Premium website templates to get you up and running{' '}
-          <span className='bg-[#FF98A2] px-1'>faster.</span>
-        </Balancer>
-      </h1>
-      <p className='text-lg'>
-        Explore the latest templates built with React and Next.js.
-      </p>
-    </div>
+    <>
+      <div className='pt-24 px-4'>
+        <h1 className='text-3xl md:text-[5vw] md:leading-[1.1] font-semibold tracking-tight pb-6'>
+          <Balancer>
+            Premium website templates to get you up and running{' '}
+            <span className='bg-[#FF98A2] px-1'>faster.</span>
+          </Balancer>
+        </h1>
+        <p className='text-lg'>
+          Explore the latest templates built with React and Next.js.
+        </p>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-10'>
+        {templates.map((t, i) => (
+          <Link
+            href='#'
+            key={i}
+          >
+            <div className='flex flex-col px-2'>
+              <div className='w-full h-full overflow-hidden'>
+                <Image
+                  src={t.img}
+                  alt={`${t.name} template on a laptop`}
+                  width={1000}
+                  height={1000}
+                  priority
+                  className='object-cover'
+                />
+              </div>
+              <p className='bg-black text-white uppercase rounded-full text-xs px-3 py-2 w-fit mb-4'>
+                {t.category}
+              </p>
+              <div className='flex justify-between'>
+                <h3 className='text-2xl font-semibold tracking-tight pb-2'>
+                  {t.name}
+                </h3>
+                <p className='tracking-tight'>{t.price}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
 import HomePage from './components/home-page'
-
+const templates = [
+  {
+    name: 'Onyx',
+    price: '$99.00',
+    category: 'Portfolio',
+    img: '/images/onyx-macbook.png',
+  },
+  {
+    name: 'Dynamic',
+    price: '$99.00',
+    category: 'Portfolio',
+    img: '/images/dynamic-macbook.png',
+  },
+  {
+    name: 'Artisan',
+    price: '$99.00',
+    category: 'Agency',
+    img: '/images/artisan-macbook.png',
+  },
+  {
+    name: 'Ghost',
+    price: '$99.00',
+    category: 'Portfolio',
+    img: '/images/ghost-macbook.png',
+  },
+  {
+    name: 'Justify',
+    price: '$99.00',
+    category: 'Portfolio',
+    img: '/images/onyx-macbook.png',
+  },
+]
 export default function Home() {
-  return <HomePage />
+  return <HomePage templates={templates} />
 }
